a4ec04eea2506099f4232f5d18012e3c3092b1a6
Paul Winter <zimin.pavel.v@gmail.com>
2026-04-13 10:50:36 +0300

    First commit

---
+++ DBInterface/EditCategory.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Sh0pDB;
+using Sh0pDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DBInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для EditCategory.xaml
+    /// </summary>
+    public partial class EditCategory : Window
+    {
+        private ShopDBContext _DB;
+
+        public EditCategory()
+        {
+            InitializeComponent();
+            _DB = new ShopDBContext();
+            FillListBox();
+        }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Category_ListBox.Items.Count > 0)
+            {
+                InputTextBox.Text = Category_ListBox.SelectedItem.ToString();
+            }
+        }
+        private void FillListBox()
+        {
+            Category_ListBox.ItemsSource = _DB.categories.ToList();        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _DB.categories.Remove(_DB.categories.First(c=>c.Type == InputTextBox.Text));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _DB.categories.Add(new Category { Type = InputTextBox.Text });
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            _DB.SaveChanges();
+            MessageBox.Show("Данные обновлены!");
+            FillListBox();
+        }
+    }
+}
---
+++ Sh0pDB/Models/Tables.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sh0pDB.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Name  - {Name}\nEmail - {Email}\nAge   - {Age} y.o.";
+        }
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return Type;
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Orders
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public User User { get; set; }
+    }
+    public class OrderItem 
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
+        public Orders Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+    }
+
+}
